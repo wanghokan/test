541364197d264f833eb2e8ba9786776147ecdd65
unknown <w52806991@gmail.com>
2020-03-20 20:03:34 +0800

    deploy test react app to github pages

---
+++ src/test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+
+    render(){
+        return(
+            <div className="App">
+                <h1>Test</h1>
+            </div>
+        );
+    }
+}
+
+export default Test;
