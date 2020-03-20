298fbe5121ef7ee27cfa9d6be7ef4ddd8a1ca4cf
unknown <w52806991@gmail.com>
2020-03-20 20:04:20 +0800

    Updates

---
+++ precache-manifest.4b29530079868876b174e92468cdbdfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8df1975719cfee8c25d52355b9fb47a",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "85646a1b79f29a3c8a10",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4bbda417c3f43ee96b8",
+    "url": "/test/static/js/2.df1ff1b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.df1ff1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85646a1b79f29a3c8a10",
+    "url": "/test/static/js/main.bbc0dedb.chunk.js"
+  },
+  {
+    "revision": "a2a118daa8ca02cff825",
+    "url": "/test/static/js/runtime-main.5ebfacb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
